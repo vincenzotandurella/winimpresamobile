4ceedcf4ed8c0e7c18c9a9d134b876b11d3d0615
sabat.russo <russosabato88@gmail.com>
2015-09-21 18:51:58 +0200

    struttura iniziale che racchiude tutte le funzionalità

---
+++ workspace/WinImpresaMobile/src/com/winimpresa/mobile/utility/Buono.java
@@ -0,0 +1,49 @@
+package com.winimpresa.mobile.utility;
+
+public class Buono {
+	
+	private String titolo;
+	private int tipologia;
+	private int status;
+	
+	public Buono(String titolo,int tipologia,int status){
+		
+		this.titolo    = titolo;
+		this.tipologia = tipologia; 
+		this.status    = status;
+	}
+
+
+	public int getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+
+	public int getTipologia() {
+		return tipologia;
+	}
+
+
+	public void setTipologia(int tipologia) {
+		this.tipologia = tipologia;
+	}
+	
+	
+	
+
+}
